1ee297b9b40548e092d1e5f4a49f0c15de47501a
steveyalcin <steven.yalcin@gmail.com>
2020-04-12 00:43:01 -0300

    Filter Search component is implemented successfully

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
-import React, { Component } from 'react';
-import FilterResults from 'react-filter-search';
+import React from 'react';
 import './App.css';
+import FilterSearch from './FilterSearch';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      value: ''
-    };
-  }
-  componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => this.setState({ data: json }));
-  }
-  handleChange = event => {
-    const { value } = event.target;
-    this.setState({ value });
-  };
-  render() {
-    const { data, value } = this.state;
-    return (
-      <div>
-        <input type="text" value={value} onChange={this.handleChange} />
-        <FilterResults
-          value={value}
-          data={data}
-          renderResults={results => (
-            <div>
-              {results.map(el => (
-                <div>
-                  <span>{el.name}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <FilterSearch />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/FilterSearch.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import FilterResults from 'react-filter-search';
+
+const FilterSearch = () => {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState('');
+
+  async function fetchData() {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users');
+    result.json().then(result => setData(result));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <FilterResults
+        value={value}
+        data={data}
+        renderResults={results => (
+          <div>
+            {results.map(item => (
+              <div key={item.id}>{item.name}</div>
+            ))}
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+export default FilterSearch;
